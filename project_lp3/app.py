c9ded4096557e11e1e490d2bc823afa5e00eba49
Gabriel Martins Rocha <gmrgabriel1@gmail.com>
2024-06-18 11:33:33 -0300

    Primeira aplicação com flask.

--- project_lp3/app.py
+++ project_lp3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from validate_docbr import CPF, CNPJ
 
 
@@ -7,15 +7,21 @@ app = Flask("My App")
 
 @app.route("/")
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html")
 
 @app.route("/contato")
 def contato():
-    return "<h1>Contato</h1>"
+    return render_template("contato.html")
 
 @app.route("/produtos")
 def produtos():
-    return "<h1>Produtos</h1>"
+    lista_produtos = [
+        {"nome": "Spray", "descricao": "Spray capilar para cabelo", "imagem": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTOGhZcS1QofGa5igCqmBoX35fdOQF-0rgpGNyUqKnLl4Ix7U4C_6ZQs7s9J176vzB3lxG1MVj1B-GiuHZ3XUJNTodvFKRTHo6azSxlfl20U_2lszrlnveELEJuL4JLMCKrFQER3g&usqp=CAc"},
+        {"nome": "Saw Palmetto", "descricao": "Saw Palmetto para Queda Capilar", "imagem": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQdFNvDzcBjo9eNzpoocPFoXJNqt_qYOa9mc0JtYhDp-E0HwM0nBtXvSG1zak7NSZ3OvksgzveY34gO4UAEIZ964Zg52u8y5hZa90RG59-jk6_96iTHmdeOBw2GTMJtH_k12ULNjpQ&usqp=CAc"},
+        {"nome": "MinoxidiI", "descricao": "MinoxidiI Tópico para Queda Capilar ", "imagem": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSHsqcxCAzYBKWywG3AVoUJf4gdOEqlKLi3tninlTwV7uVYD9YW-bYxAbN9tFqf8staimGeTmxf5VJPLXiNDVIfzBncA_k54A2CYOEP2dzV8mfxZrBpoyb84Su_UlLNqrKGmb42U2I&usqp=CAc"}
+    ]
+
+    return render_template("produtos.html", produtos = lista_produtos)
 
 
 @app.route("/servicos")
