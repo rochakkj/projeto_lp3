48e7b3b808ca2dc1715030da437ed4bf4f0a05cc
Gabriel Martins Rocha <gmrgabriel1@gmail.coma>
2024-06-25 11:32:09 -0300

    Configurando cadastro de produtos e adicionando static, para o style

--- project_lp3/app.py
+++ project_lp3/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from validate_docbr import CPF, CNPJ
 
+lista_produtos = [
+        {"nome": "Spray", "descricao": "Spray capilar para cabelo", "imagem": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTOGhZcS1QofGa5igCqmBoX35fdOQF-0rgpGNyUqKnLl4Ix7U4C_6ZQs7s9J176vzB3lxG1MVj1B-GiuHZ3XUJNTodvFKRTHo6azSxlfl20U_2lszrlnveELEJuL4JLMCKrFQER3g&usqp=CAc"},
+        {"nome": "Saw Palmetto", "descricao": "Saw Palmetto para Queda Capilar", "imagem": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQdFNvDzcBjo9eNzpoocPFoXJNqt_qYOa9mc0JtYhDp-E0HwM0nBtXvSG1zak7NSZ3OvksgzveY34gO4UAEIZ964Zg52u8y5hZa90RG59-jk6_96iTHmdeOBw2GTMJtH_k12ULNjpQ&usqp=CAc"},
+        {"nome": "MinoxidiI", "descricao": "MinoxidiI Tópico para Queda Capilar ", "imagem": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSHsqcxCAzYBKWywG3AVoUJf4gdOEqlKLi3tninlTwV7uVYD9YW-bYxAbN9tFqf8staimGeTmxf5VJPLXiNDVIfzBncA_k54A2CYOEP2dzV8mfxZrBpoyb84Su_UlLNqrKGmb42U2I&usqp=CAc"}
+    ]
 
 
 app = Flask("My App")
@@ -15,11 +20,7 @@ def contato():
 
 @app.route("/produtos")
 def produtos():
-    lista_produtos = [
-        {"nome": "Spray", "descricao": "Spray capilar para cabelo", "imagem": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTOGhZcS1QofGa5igCqmBoX35fdOQF-0rgpGNyUqKnLl4Ix7U4C_6ZQs7s9J176vzB3lxG1MVj1B-GiuHZ3XUJNTodvFKRTHo6azSxlfl20U_2lszrlnveELEJuL4JLMCKrFQER3g&usqp=CAc"},
-        {"nome": "Saw Palmetto", "descricao": "Saw Palmetto para Queda Capilar", "imagem": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQdFNvDzcBjo9eNzpoocPFoXJNqt_qYOa9mc0JtYhDp-E0HwM0nBtXvSG1zak7NSZ3OvksgzveY34gO4UAEIZ964Zg52u8y5hZa90RG59-jk6_96iTHmdeOBw2GTMJtH_k12ULNjpQ&usqp=CAc"},
-        {"nome": "MinoxidiI", "descricao": "MinoxidiI Tópico para Queda Capilar ", "imagem": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSHsqcxCAzYBKWywG3AVoUJf4gdOEqlKLi3tninlTwV7uVYD9YW-bYxAbN9tFqf8staimGeTmxf5VJPLXiNDVIfzBncA_k54A2CYOEP2dzV8mfxZrBpoyb84Su_UlLNqrKGmb42U2I&usqp=CAc"}
-    ]
+    
 
     return render_template("produtos.html", produtos = lista_produtos)
 
@@ -48,5 +49,24 @@ def gerarCNPJ():
     cnpj = CNPJ()
 
     return render_template("gerar-cnpj.html", cnpjs = cnpj.generate(False))
+
+#get
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+
+    return render_template("cadastro_produto.html")
+
+#post
+@app.route("/produtos", methods=['POST'])
+def salvar_produto():
+    #Pegando valores do form, nome, descricao, etc
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+    imagem = request.form["imagem"]
+    #novo produto
+    produto = {"nome": nome, "descricao": descricao, "imagem": imagem}
+    #novo produto na lista de produtos
+    lista_produtos.append(produto)
+    return render_template("produtos.html", produtos = lista_produtos)
 
 app.run()
